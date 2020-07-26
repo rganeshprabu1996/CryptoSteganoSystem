0216f95d8cf6dc81b1f642ee0607248093bfd144
GANESHPRABU R <54024597+rganeshprabu1996@users.noreply.github.com>
2020-07-26 09:26:59 +0530

    Delete DDecryptionElgamal.py

--- DDecryptionElgamal.py
+++
@@ -1,8 +0,0 @@
-import Main as m
-
-def decrypt(en_msg,p,key,q):
-    dr_msg = []
-    h = m.power(p, key, q) 
-    for i in range(0, len(en_msg)): 
-        dr_msg.append(chr(int(en_msg[i]/h)))         
-    return dr_msg 
